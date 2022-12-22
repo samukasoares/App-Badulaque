277eb628bc8ee15d260ba7f1853645b2c31d20f9
Samuel <samuel.gsoares1@gmail.com>
2022-12-21 22:07:52 -0300

    Calculos Custos e Mostrar nos TextViews

--- app/src/main/java/com/example/badulaqueenterprise/Evento.java
+++ app/src/main/java/com/example/badulaqueenterprise/Evento.java
@@ -1,2 +1,231 @@
-package com.example.badulaqueenterprise;public class Evento {
+package com.example.badulaqueenterprise;
+
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Evento {
+
+    int NumConvidados, MesesAteEvento;
+
+    public Evento(int NumConvidados, int MesesAteEvento) {
+        this.NumConvidados = NumConvidados;
+        this.MesesAteEvento = NumConvidados;
+    }
+
+    public double fIPCA (){
+        double fIPCA = 0;
+        double Y = MesesAteEvento / MainActivity.K_PERIODO;
+        fIPCA = (Math.pow(1 + MainActivity.K_IPCA / 100, Y ))- 1;
+
+        if (fIPCA<0){
+            fIPCA = 0;
+        } else{
+            fIPCA = fIPCA;
+        }
+        return fIPCA;
+    }
+
+    public double fMargemContr (){
+        return 0.14 - NumConvidados * 0.0004;
+    }
+
+    public int fNumeroGarcom (){
+        return NumConvidados/20;
+    }
+
+    public int fNumAuxCozinha (){
+        int auxiliar;
+
+        if (NumConvidados >= 120){
+            auxiliar = 3;
+        } else if (NumConvidados >= 80){
+            auxiliar = 2;
+        } else{
+            auxiliar = 1;
+        }
+        return auxiliar;
+    }
+
+    public int fNumCozinheira(){
+        int cozinheira;
+        if(NumConvidados > 60){
+            cozinheira = 2;
+        }else{
+            cozinheira = 1;
+        }
+        return cozinheira;
+    }
+
+    public double fCustoCozinha(){
+        double v = 0;
+        if(NumConvidados >= 120){
+            v = CustoMoCozinha_120();
+        } else if (NumConvidados >= 80){
+            v = CustoMoCozinha_80();
+        } else if (NumConvidados >= 60){
+            v = CustoMoCozinha_60();
+        } else{
+            CustoMoCozinha_min();
+        }
+        return v;
+    }
+
+    public int CustoMoCozinha_min(){
+        return MainActivity.K_CHEF + MainActivity.K_COZINHEIRA;
+    }
+
+    public int CustoMoCozinha_60(){return MainActivity.K_CHEF + MainActivity.K_COZINHEIRA + MainActivity.K_AuxCozinha;}
+
+    public int CustoMoCozinha_80(){
+        return MainActivity.K_CHEF + 2 * MainActivity.K_COZINHEIRA + 1 * MainActivity.K_AuxCozinha;
+    }
+
+    public int CustoMoCozinha_120(){
+        return MainActivity.K_CHEF + 2 * MainActivity.K_COZINHEIRA + 3 * MainActivity.K_AuxCozinha;
+    }
+
+    public int fNumBarman (){
+        int barman = 1;
+
+        if(NumConvidados >=250){
+            barman = 5;
+        }else if (NumConvidados >=200){
+            barman = 4;
+        } else if(NumConvidados >= 150){
+            barman = 3;
+        }else if (NumConvidados >=100){
+            barman = 2;
+        }
+        return barman;
+    }
+
+    public double fCustoMoBar(CheckBox bartender){
+        double vCustoBar = 0;
+        if(bartender.isChecked()){
+            vCustoBar = fNumBarman() * MainActivity.K_BARMAN;
+        }else{
+            vCustoBar = 0;
+        }
+        return vCustoBar;
+    }
+
+
+    public double fCustoBar(CheckBox bartender, Spinner packBar){
+        double custo = 0;
+        String pacoteBar = packBar.getSelectedItem().toString();
+        if(bartender.isChecked()){
+            switch (pacoteBar) {
+                case "Bronze":
+                    custo = MainActivity.K_CUSTOBRONZE * NumConvidados;
+                    break;
+                case "Prata":
+                    custo = MainActivity.K_CUSTOPRATA * NumConvidados;
+                    break;
+                case "Ouro":
+                    custo = MainActivity.K_CUSTOOURO * NumConvidados;
+                    break;
+            }
+        }else{
+            custo = 0;
+        }
+        return custo;
+    }
+
+    public double fCustoCervejaChopp(CheckBox cerveja, CheckBox chopp, Spinner tipoCerveja) {
+        double custo = 0;
+        String tipoCervejaTexto = tipoCerveja.getSelectedItem().toString();
+
+        if (cerveja.isChecked()) {
+            switch (tipoCervejaTexto) {
+                case "Brahma/Skol":
+                    custo = MainActivity.K_CERVEJA1;
+                    break;
+                case "Original":
+                    custo = MainActivity.K_CERVEJA2;
+                    break;
+                case "Heineken":
+                    custo = MainActivity.K_CERVEJA3;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Cerveja Inválida");
+            }
+        } else if (chopp.isChecked()) {
+            custo = MainActivity.K_CHOPP;
+        } else{
+            custo = 0;
+        }
+
+        return custo;
+    }
+
+    public double fCustoMensalMo(){
+        return MainActivity.K_MO / MainActivity.K_EVENTOS;
+    }
+
+    public double fCustoPublicidade(){
+        return MainActivity.K_PUBLICIDADE/MainActivity.K_EVENTOS;
+    }
+
+    public double fCustoEcad(){
+        return MainActivity.K_ECAD / MainActivity.K_EVENTOS;
+    }
+    public double fCustoCPFL(){
+        return MainActivity.K_CPFL / MainActivity.K_EVENTOS;
+    }
+
+    public double fCustoInformatica(){
+        return MainActivity.K_INFORMATICA / MainActivity.K_EVENTOS;
+    }
+
+    public double fCustoContador(){
+        return MainActivity.K_CONTADOR / MainActivity.K_EVENTOS;
+    }
+
+    public double fTotalFixo(){
+        return  fCustoContador() + fCustoCPFL() + fCustoEcad() + fCustoInformatica() + fCustoMensalMo() + fCustoPublicidade();
+    }
+
+    public double fValorRetirada(){
+        return Math.round(13.33 * NumConvidados + 1666.7);
+    }
+
+    public double fCustoVariavel(){
+        return (MainActivity.K_Lavanderia + MainActivity.K_GLP);
+    }
+
+    public double fCustoCerimonia(CheckBox cerimonia){
+        double custo = 0;
+
+        if (cerimonia.isChecked()){
+            custo = MainActivity.K_CUSTOCERIMONIA;
+        } else{
+            custo = 0;
+        }
+        return custo;
+    }
+
+    public double descontoDiaSemana (Calendar evento){
+        double desconto = 0.8;
+        String diaEvento = MainActivity.NomeDoMes(evento.DAY_OF_WEEK, 0);
+
+        switch (diaEvento){
+            case "sábado":
+                desconto = 1;
+                break;
+            case "sexta":
+                desconto = 0.9;
+                break;
+            case "domingo":
+                desconto = 0.85;
+                break;
+        }
+        return desconto;
+    }
+
+
+
 }
--- app/src/main/java/com/example/badulaqueenterprise/MainActivity.java
+++ app/src/main/java/com/example/badulaqueenterprise/MainActivity.java
@@ -1,19 +1,14 @@
 package com.example.badulaqueenterprise;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.ContentInfo;
-import android.view.OnReceiveContentListener;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -22,7 +17,6 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -34,13 +28,84 @@ import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    final public static int K_PERIODO = 25;
-
     //Data Atual
     Date dataHoraAtual = new Date();
 
-    // Numero de meses até o evento
-    private long nm;
+    //CUSTOS
+    final public static int faxina = 120;
+
+
+    //ESTIMATIVAS
+    final public static int K_EVENTOS = 35;
+    final public static int K_CONVIDADOS = 140;
+
+    //CUSTOS FIXOS ANO
+    final public static int K_SINAL = 2000;
+    final public static int K_CONTADOR = 2160;
+    final public static int K_MO = 31200;
+    final public static int K_PUBLICIDADE = 6000;
+    final public static int K_ECAD = 9600;
+    final public static int K_INFORMATICA = 2400;
+    final public static int K_CPFL = 28000;
+    final public static int K_SAM = 12000;
+
+
+
+    //BUFFET
+    final public static int K_COZINHEIRA = 300;
+    final public static int K_CHEF = 350;
+    final public static int K_AuxCozinha = 180;
+    final public static int K_Precozinha = 150;
+    final public static int K_PERDAS = 10;
+    final public static int K_CERVEJA = 6;
+
+    final public static double custoCardapioBronze = 60;
+    final public static double custoCardapioPrata = 120;
+    final public static double custoCardapioOuro = 0;
+    final public static double custoCardapioChurrasco = 0;
+    final public static double custoCardapioMineira = 0;
+    final public static double custoCardapioCoquetel = 0;
+    final public static double custoCardapioBoteco = 0;
+
+
+    //ESPAÇO
+    final public static int K_ESPACO = 6400;
+    final public static int K_GARCOM = 150;
+    final public static int K_FAXINEIRA = 150;
+    final public static int K_AJUDANTE = 100;
+    final public static int K_BANHEIRO = 100;
+    final public static int K_SUPERVISAO = 350;
+    final public static int K_CUSTOCABINE = 800;
+    final public static int K_CUSTOCERIMONIA = 800;
+    final public static int K_KIDS = 370;
+    final public static int K_HE = 15; //Hora Extra por pessoa
+    final public static double K_Lavanderia = 1.2; //Por Pessoa
+    final public static double K_GLP = 1.2;
+    final public static int K_FAXINA = 200;
+    final public static int K_AuxCozMin = 1;
+    final public static int K_CozinheiraMin = 1;
+
+    //BAR
+    final public static int K_BRONZE = 20;
+    final public static int K_PRATA = 24;
+    final public static int K_OURO = 28;
+    final public static int K_SEMALCOOL = 16;
+    final public static double K_PERSONAL = 4.5;
+    final public static int K_PACKSEMALCOOL = 15;
+    final public static int K_DRINKPP = 2;
+    final public static int K_BARMAN = 150;
+    final public static double K_CUSTOBRONZE = 7.64;
+    final public static double K_CUSTOPRATA = 9.27;
+    final public static double K_CUSTOOURO = 11.76;
+    final public static double K_CERVEJA1 = 9.911;
+    final public static double K_CERVEJA2 = 11.339;
+    final public static double K_CERVEJA3 = 14.161;
+    final public static double K_CHOPP = 12.113;
+
+    //Financeiro
+    final public static int K_FUTURA = 25;
+    final public static int K_PERIODO = 12;
+    final public static int K_IPCA = 9;
 
     //Formatações de Data
     SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
@@ -50,11 +115,17 @@ public class MainActivity extends AppCompatActivity {
     DatePickerDialog dpd;
     Calendar c;
     ImageView btnCal;
-    TextView textoData, dataExtenso , meses, valorEstrutura, valorBronze, valorPrata, valorOuro, valorMineira, valorCoquetel, valorChurrasco, valorBoteco ;
+    public TextView textoData, dataExtenso , meses, valorEstrutura, valorBronze, valorPrata, valorOuro, valorMineira, valorCoquetel, valorChurrasco, valorBoteco ;
     EditText numConvidados;
     Date dt;
     String dataEvento;
-    CheckBox cerveja, chopp;
+    public CheckBox cerveja, chopp, bartender, cerimonia;
+    Spinner spinner, spinnerPackBar;
+
+    public TextView lblBronze, lblPrata;
+
+
+    private long nm; //Numero de meses até o evento
 
     public void limparCampos(){
         textoData.setText("");
@@ -74,7 +145,6 @@ public class MainActivity extends AppCompatActivity {
     public void btnLimparCampos (View view){
         limparCampos();
     }
-
 
     public static String NomeDoMes(int i, int tipo) {
         String mes[] = {"jan.", "fev.", "mar.", "abr.", "maio", "jun.", "jul.", "ago.", "set.", "out.", "nov.", "dez."};
@@ -117,7 +187,7 @@ public class MainActivity extends AppCompatActivity {
         return camposValidados;
     }
 
-    //Validar se número de meses é menor que K_PERIODO
+    //Validar se número de meses é menor que FUTURA
     public boolean validarMeses(String data){
         boolean mesesValidados = true;
 
@@ -128,19 +198,108 @@ public class MainActivity extends AppCompatActivity {
             LocalDate dataAtual = LocalDate.now();
             nm = ChronoUnit.MONTHS.between(dataAtual,dataF);
         }
-        if(nm > K_PERIODO){
+        if(nm > K_FUTURA){
             mesesValidados = false;
         }
         return mesesValidados;
     }
 
     //Ação do botão confirmar
-
+    @SuppressLint("SetTextI18n")
     public void botaoConfirmar (View view){
         boolean camposValidados = validarCampos();
 
         if(camposValidados){
-            Toast.makeText(getApplicationContext(), "Rodar Código!", Toast.LENGTH_SHORT).show();
+            int nc = Integer.parseInt(numConvidados.getText().toString()); //Numero Convidados
+            int meses = (int) nm;
+
+            Evento evento = new Evento(nc,meses);
+            double valorIPCA = evento.fIPCA();
+            double valorMargemContr = evento.fMargemContr();
+            double valorLimpeza = faxina;
+            double valorSupervisão = K_SUPERVISAO;
+            double valorAjudante = K_AJUDANTE;
+            int numGarcom = evento.fNumeroGarcom();
+            double valorGarcom = numGarcom * K_GARCOM;
+            int valorKids = K_KIDS;
+
+            double valorTotalFixo = evento.fTotalFixo();
+
+            double valorCustoEspacoMO = valorSupervisão + valorGarcom + valorLimpeza + valorAjudante + valorKids;
+            double valorCustoEspaco =  valorTotalFixo + valorCustoEspacoMO;
+
+
+            double valorTotalCozinha = evento.fCustoCozinha();
+
+            double valorRetirada = evento.fValorRetirada();
+            double valorCustoCervejaChopp = evento.fCustoCervejaChopp(cerveja,chopp,spinner);
+            double valorCustoVariavel = evento.fCustoVariavel();
+            double valorCerimonia = evento.fCustoCerimonia(cerimonia);
+
+
+            //Cardapio bronze = new Cardapio("Bronze", custoCardapioBronze);
+           //bronze.fCustoBuffet(valorCustoVariavel,valorTotalCozinha,valorCustoCervejaChopp,nc);
+
+            String[] cardapioNome = {"Bronze", "Prata", "Ouro", "Churrasco", "Mineira", "Coquetel", "Boteco"};
+            double[] cardapioPreco = {custoCardapioBronze,custoCardapioPrata,custoCardapioOuro,custoCardapioChurrasco,custoCardapioMineira,custoCardapioCoquetel,custoCardapioBoteco};
+
+            double valorEspaco = 0;
+            double valorPPFinal;
+            for (int i = 0; i<cardapioPreco.length; i++){
+                double valorCustoBuffet = valorCustoVariavel + valorTotalCozinha + (cardapioPreco[i] + valorCustoCervejaChopp) * nc;
+                double valorCustoProposta = (valorCustoEspaco + cardapioPreco[i])/1-valorIPCA;
+                double valorCustoBar = ((evento.fCustoBar(bartender, spinnerPackBar) + evento.fCustoMoBar(bartender))/(1-valorIPCA));
+                double valorCustoContrato = (valorCustoBar + valorCustoProposta)/(1-valorIPCA);
+                double margc = 1 - valorMargemContr;
+                double valorEspacoInicial = valorCustoEspaco / margc;
+                double valorBuffetInicial = valorCustoBuffet / margc + valorRetirada;
+                double valorPPInicial = valorBuffetInicial/nc;
+                double vppInicial = valorBuffetInicial / nc;
+                double vpp_inc;
+                if(meses>0){
+                    vpp_inc = vppInicial / (1-valorIPCA) - vppInicial;
+                }else{
+                    vpp_inc = 0;
+                }
+                double desconto = evento.descontoDiaSemana(c);
+
+                if(nc >= 150){
+                    valorEspaco = K_ESPACO;
+                }else if(nc>=100){
+                    valorEspaco = 5800;
+                }else{
+                    valorEspaco = 5000;
+                }
+                double ved = Math.round(valorEspaco * desconto);
+                double vpp_dec = (ved - valorCustoEspaco) / nc;
+                valorPPFinal = Math.round(valorPPInicial + vpp_inc - vpp_dec);
+
+                switch (i){
+                    case 0:
+                        valorBronze.setText("R$" + Double.toString(valorPPFinal));
+                        break;
+                    case 1:
+                        valorPrata.setText("R$" + Double.toString(valorPPFinal));
+                        break;
+                    case 2:
+                        valorOuro.setText("R$" + Double.toString(valorPPFinal));
+                        break;
+                    case 3:
+                        valorChurrasco.setText("R$" + Double.toString(valorPPFinal));
+                        break;
+                    case 4:
+                        valorMineira.setText("R$" + Double.toString(valorPPFinal));
+                        break;
+                    case 5:
+                        valorCoquetel.setText("R$" + Double.toString(valorPPFinal));
+                        break;
+                    case 6:
+                        valorBoteco.setText("R$" + Double.toString(valorPPFinal));
+                        break;
+                }
+            }
+            valorEstrutura.setText(Double.toString(valorEspaco));
+
         }else{
             Toast.makeText(getApplicationContext(), "Preencher Campos!", Toast.LENGTH_SHORT).show();
         }
@@ -169,14 +328,28 @@ public class MainActivity extends AppCompatActivity {
         valorMineira = (TextView) findViewById(R.id.editTextValorMineira);
         cerveja = (CheckBox) findViewById(R.id.checkBoxCerveja);
         chopp = (CheckBox) findViewById(R.id.checkBoxChopp);
+        bartender = (CheckBox) findViewById(R.id.checkBoxBartender);
+        cerimonia = (CheckBox) findViewById(R.id.checkBoxCerimonia);
 
-        //Popular ComboBox (Spinner)
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        lblBronze = (TextView) findViewById(R.id.textBronze);
+        lblPrata = (TextView) findViewById(R.id.textPrata);
+
+        //Popular ComboBox(Spinner) Cerveja
+        spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.tiposCerveja, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setEnabled(false);
+
+        //Popular ComboBox(Spinner) Pacotes Bar
+        spinnerPackBar = findViewById(R.id.spinnerBar);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.packsBar, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPackBar.setAdapter(adapter1);
+        spinnerPackBar.setEnabled(false);
+
 
 
         //Listener para botão do calendário
@@ -229,7 +402,7 @@ public class MainActivity extends AppCompatActivity {
                         dataExtenso.setText("");
                         meses.setText("");
                         textoData.setText("");
-                        throw new Exception("Número de meses acima de " + K_PERIODO + "!");
+                        throw new Exception("Número de meses acima de " + K_FUTURA + "!");
                     }else if (date.before(dataHoraAtual)){
                         dataExtenso.setText("");
                         meses.setText("");
@@ -258,8 +431,6 @@ public class MainActivity extends AppCompatActivity {
                     chopp.setEnabled(true);
                     spinner.setEnabled(false);
                 }
-
-
             }
         });
 
@@ -275,6 +446,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        bartender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bartender.isChecked()){
+                    spinnerPackBar.setEnabled(true);
+                }else{
+                    spinnerPackBar.setEnabled(false);
+                }
+            }
+        });
+
+
+
+
 
     }
 }
